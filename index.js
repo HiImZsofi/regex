a495fc911bcff43863992328098410e0e0642619
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-26 14:31:05 +0200

    check card

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let username = /^[a-zA-Z]+/
 let adress = /^([a-zA-Z]+(á[a-zA-Z]+)+).*[a-zA-Z]+,.*[0-9]+/
 let number = /^[0-9]+/
+let creditNumber = /^(?:5[1-5][0-9]{14})$/
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -9,11 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
         var countryInput = document.getElementById('country').value;
         var cityInput = document.getElementById('city').value;
         var adressInput = document.getElementById('adress').value;
+        var codeInput = document.getElementById('code').value;
+        var cardInput = document.getElementById('card').value;
 
 
         checkUsername(usernameInput)
         checkCountry(countryInput)
         checkCity(cityInput)
+        checkCode(codeInput)
+        checkCard(cardInput)
     })
 
     
@@ -59,6 +64,15 @@ function checkAdress(input){
 function checkCode(input){
     if(!(number.test(input)) || input == null) {
         document.getElementById('codea').innerHTML = "Nem jo"
+       }
+       else {
+        return console.log("ok")
+       }
+}
+
+function checkCode(input){
+    if(!(creditNumber.test(input)) || input == null) {
+        document.getElementById('carda').innerHTML = "Nem jo"
        }
        else {
         return console.log("ok")
