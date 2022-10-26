4b64a04393cb4a773de27c1bc40b37f66aa6684a
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-26 14:26:19 +0200

    check adress

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let username = /^[a-zA-Z]+/
+let adress = /^([a-zA-Z]+(á[a-zA-Z]+)+).*[a-zA-Z]+,.*[0-9]+/
 
 document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('submit').addEventListener('click', () => {
         var usernameInput = document.getElementById('name').value;
+        var countryInput = document.getElementById('country').value;
+        var cityInput = document.getElementById('city').value;
+        var adressInput = document.getElementById('adress').value;
+
+
         checkUsername(usernameInput)
+        checkCountry(countryInput)
+        checkCity(cityInput)
     })
 
     
@@ -19,3 +27,31 @@ function checkUsername(input){
     return console.log("ok")
    }
 }
+
+function checkCountry(input){
+    if(!(username.test(input)) || input == null) {
+     document.getElementById('countrya').innerHTML = "Nem jo"
+    }
+    else {
+     return console.log("ok")
+    }
+ }
+
+ function checkCity(input){
+    if(!(username.test(input)) || input == null) {
+     document.getElementById('citya').innerHTML = "Nem jo"
+    }
+    else {
+     return console.log("ok")
+    }
+ }
+
+function checkAdress(input){
+    if(!(adress.test(input)) || input == null) {
+        document.getElementById('adressa').innerHTML = "Nem jo"
+       }
+       else {
+        return console.log("ok")
+       }
+}
+
